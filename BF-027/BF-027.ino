d19ead48a65975e0b046837ff88bf2c8d854abbf
Yoshiyuki Uehara <uehara00@outlook.jp>
2021-05-15 20:45:07 +0900

    Update BF-027.ino
    selection of M5Stack/M5Atom

--- BF-027/BF-027.ino
+++ BF-027/BF-027.ino
@@ -2,20 +2,32 @@
 // BF-027 PCF8563 RTC for Grove I2C
 // test and example
 
-#include <M5Atom.h>
+// ----- selection of controllers(1/3) -----
+// for M5Stack
+#include <M5Stack.h>
+// for M5Atom
+//#include <M5Atom.h>
+// ----- selection of controllers(1/3) end -----
+
 #include "BF_Wifi.h"
 #include "BF_RtcxNtp.h"
 #include "BF_Pcf8563Test.h"
 
-// for M5Atom
+// ----- selection of controllers(2/3) -----
+// for M5Stack
+const bool lcd_enable(true);
+const bool sd_enable(true);
 const bool serial_enable(true);
 const bool i2c_enable(true);
-const bool display_enable(true);
-
-// for I2C
-const int wire_scl(21);       // GPIO21
-const int wire_sda(25);       // GPIO25
-const int wire_freq(100000);  // 100kHz
+// for M5Atom
+//const bool serial_enable(true);
+//const bool i2c_enable(true);
+//const bool display_enable(true);
+// for I2C of M5Atom
+//const int wire_scl(21);       // GPIO21
+//const int wire_sda(25);       // GPIO25
+//const int wire_freq(100000);  // 100kHz
+// ----- selection of controllers(2/3) end -----
 
 // for loop control
 const int loop_ms(20);          // 20ms
@@ -25,8 +37,13 @@ int interval_last_ms(0);
 
 void setup()
 {
-  M5.begin(serial_enable, !i2c_enable, !display_enable);
-  Wire.begin(wire_sda, wire_scl, wire_freq);
+  // ----- selection of controllers(3/3) -----
+  // for M5Stack
+  M5.begin(lcd_enable, sd_enable, serial_enable, i2c_enable);
+  // for M5Atom
+  //M5.begin(serial_enable, !i2c_enable, !display_enable);
+  //Wire.begin(wire_sda, wire_scl, wire_freq);
+  // ----- selection of controllers(3/3) end -----
 
   // RTCx PCF8563: set I2C connected
   rtcx.Begin(Wire);
